0e02c2fe23e9545c6677eef72fd5d757547c32aa
yayamayaya <semenov.aleksandr@phystech.edu>
2024-03-24 20:06:12 +0300

    -

--- src/expression_reader.cpp
+++ src/expression_reader.cpp
@@ -27,70 +27,77 @@ node_t *create_tree(char *buffer)
     char character = 0;
     int scanned = 0;
     node_t *node = NULL;
-    pos++;
 
     LOG("> current pos: <%d>, the rest of the expression: <<%s>>\n", pos, buffer + pos);
 
-    /*if (buffer[pos] == '(')
+    if (buffer[pos] == '(')
     {
         LOG("> Found opening bracket on pos: %d\n, rest is: <<%s>>", pos, buffer + pos);
-        
         pos++;
-    }*/
-    if(buffer[pos] == ')')
-    {
-        LOG("> found empty bracket on pos: %d\n, rest is: <<%s>>", pos, buffer + pos);
-        pos++;
-        return NULL;
-    }
 
-    if (calloc_for_node(&node))
-        return NULL;
-
-    if (buffer[pos] == '(')
-        node->l = create_tree(buffer);        
-
-    sscanf(buffer + pos, "%lf%n", &number, &scanned);  
-    if (scanned)
-    {
-        LOG("> number founded: %lf, on pos: '%d'\n", number, pos);
-        pos += scanned;
-        node->data.number = number;
-        node->data_type = 1;
-    }
-    else
-    {
-        sscanf(buffer + pos, "%c%n", &character, &scanned);
-        LOG("> scanned character is: '%c', on pos : '%d'\n", character, pos);
-        int operation = detect_operation(character);
-        if (operation)
+        if(buffer[pos] == ')')
         {
-            LOG("> operation was founded, it's code is '%x'", operation);
+            LOG("> found empty bracket on pos: %d\n, rest is: <<%s>>", pos, buffer + pos);
             pos++;
-            node->data.operation = operation;
-            node->data_type = 2;
+            return NULL;
         }
-        /*if (detect_variable(character))
-        {
-            code 
-        }*/
-    }
-    if (buffer[pos] == '(')
-        node->r = create_tree(buffer);        
 
-    if (buffer[pos] == ')')
-    {
-        LOG("> found closing bracket\n");
-        pos++;
+        if (calloc_for_node(&node))
+            return NULL;
+
+        if (buffer[pos] == '(')
+            node->l = create_tree(buffer);        
+
+        sscanf(buffer + pos, "%lf%n", &number, &scanned);  
+        if (scanned)
+        {
+            LOG("> number founded: %lf, on pos: '%d'\n", number, pos);
+            pos += scanned;
+            node->data.number = number;
+            node->data_type = 1;
+        }
+        else
+        {
+            sscanf(buffer + pos, "%c%n", &character, &scanned);
+            LOG("> scanned character is: '%c', on pos : '%d'\n", character, pos);
+            int operation = detect_operation(character);
+            if (operation)
+            {
+                LOG("> operation was founded, it's code is '%x'", operation);
+                pos++;
+                node->data.operation = operation;
+                node->data_type = 2;
+            }
+            /*if (detect_variable(character))
+            {
+                code 
+            }*/
+        }
+        if (buffer[pos] == '(')
+            node->r = create_tree(buffer);        
+
+        if (buffer[pos] == ')')
+        {
+            LOG("> found closing bracket\n");
+            pos++;
+        }
+        else
+        {
+            LOG(">>> syntax error, please check your spelling\n");
+            return NULL;
+        }
+
+        LOG(">> node was created with the pointer: %p\n", node);     
+        return node;           
     }
     else
     {
-        LOG(">>> syntax error, please check your spelling\n");
+        LOG(">>> syntax error, please check your spelling%40s\n", "[error]");
         return NULL;
     }
 
-    LOG(">> node was created with the pointer: %p\n", node);
-    return node;
+
+
 }
 
 int calloc_for_node(node_t **node)
