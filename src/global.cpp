acabfe20ff690bcd3d255ccae1ec3a7e44d2a0b8
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-01 01:09:07 +0300

    добавлены новые команды

--- src/global.cpp
+++ src/global.cpp
@@ -1,12 +1,27 @@
-#include "headers/main_header.h"
+#include "headers/main.h"
 
 FILE *diff_log = NULL;
 
 operation_t operations[OPERATIONS_NUMBER] = 
 {
-    {"+", PLUS},
-    {"-", MINUS},
+    {"+", ADD},
+    {"-", SUB},
     {"*", MULT},
     {"/", DIV},
     {"^", POW},
+    {"sin", SIN},
+    {"cos", COS},
+    {"tg", TAN},
+    {"ctg", COT},
+    {"sh", SH},
+    {"ch", CH},
+    {"th", TH},
+    {"cth", CTH},
+    {"log", LOG},
+    {"ln", LN},
+    {"lg", LG},
+    {"arcsin", ASIN},
+    {"arccos", ACOS},
+    {"arctg", ATAN},
+    {"arcctg", ACOT}
 };
--- src/headers/operations.h
+++ src/headers/operations.h
@@ -1,7 +1,7 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
-#define OPERATIONS_NUMBER 5
+#define OPERATIONS_NUMBER 20
 
 typedef struct
 {
@@ -11,11 +11,31 @@ typedef struct
 
 enum OPERATIONS
 {
-    PLUS        = 0x01,
-    MINUS       = 0x02,
+    ADD         = 0x01,
+    SUB         = 0x02,
     MULT        = 0x03,
     DIV         = 0x04,
     POW         = 0x05,
+
+    SIN         = 0x10,
+    COS         = 0x11,
+    TAN         = 0x12,
+    COT         = 0x13,
+
+    SH          = 0x20,
+    CH          = 0x21,
+    TH          = 0x22,
+    CTH         = 0x23,
+
+    LOG         = 0x30,
+    LN          = 0x31,
+    LG          = 0x32,
+
+    ASIN        = 0x40,
+    ACOS        = 0x41,
+    ATAN        = 0x42,
+    ACOT        = 0x43,
+
 };
 
 /*#define SUM +
