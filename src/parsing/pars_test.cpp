9fa63734ff87ad998526e0f0e0405c01dd29b68f
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-03 17:50:09 +0300

    создание графов черзе дефайн

--- src/parsing/pars_test.cpp
+++ src/parsing/pars_test.cpp
@@ -6,11 +6,12 @@ int main(int argc, char const *argv[])
 
     token_t *token_arr = NULL;
     tokenizator(&token_arr, "data/expression_to_diff.txt");
-    printf("%p\n", token_arr);
+    LOG("%p\n", token_arr);
 
     node_t *root = pars_expr(token_arr);
 
-    create_gparh_code(root, EXPR);
+    clear_all_png();
+    _CREATE_GRAPH(root, EXPR);
     
     free(token_arr);
     tree_kill(root);
