950a87a0066c5dc0d9d7e871e405710e578bcc19
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-03 15:48:46 +0300

    сделал дефайн для открытия логфайлов

--- src/headers/log.h
+++ src/headers/log.h
@@ -3,7 +3,28 @@
 
 #include <stdio.h>
 
+#ifdef DEBUG
+
 extern FILE *diff_log;
+
+#define _OPEN_LOG(arg)\
+    diff_log = fopen(arg, "wb");                             \
+    if (!diff_log)                                                          \
+    {                                                                       \
+        fprintf(stderr, ">>> Couldn't open logfile.");                      \
+        return ERR;                                                         \
+    }                                                                       \
+    setbuf(diff_log, NULL)
+
+#define _CLOSE_LOG()\
+    fclose(diff_log)
+
 #define LOG(...)  fprintf(diff_log, __VA_ARGS__)
+
+#else
+#define _OPEN_LOG(arg)
+#define _CLOSE_LOG()
+#define LOG(...)
+#endif
 
 #endif
