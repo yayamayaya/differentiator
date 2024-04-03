a471118ba5cdc1140b4516a79e3ae1b8b4ab74b6
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-03 11:54:16 +0300

    добавлена функция печати узла, запись операции и переменной теперь находятся в одной функции

--- src/create_node.cpp
+++ src/create_node.cpp
@@ -1,6 +1,8 @@
 //#include "headers/main.h"
 #include "headers/create_node.h"
 
+void node_print(const node_t *node);
+
 node_t *create_node(double data, int data_type, node_t *left_node, node_t *right_node)
 {
     node_t *node = (node_t *)calloc(1, sizeof(node_t));
@@ -15,10 +17,12 @@ node_t *create_node(double data, int data_type, node_t *left_node, node_t *right
     node->l = left_node;
     node->r = right_node;
 
+    node_print(node);
+
     return node;
 }
 
-node_t *create_node(enum OPERATIONS data, int data_type, node_t *left_node, node_t *right_node)
+/*node_t *create_node(enum OPERATIONS data, int data_type, node_t *left_node, node_t *right_node)
 {
     node_t *node = (node_t *)calloc(1, sizeof(node_t));
     if (!node)
@@ -33,10 +37,12 @@ node_t *create_node(enum OPERATIONS data, int data_type, node_t *left_node, node
     node->r = right_node;
 
     return node;
-}
+}*/
 
 node_t *create_node(const char data, int data_type, node_t *left_node, node_t *right_node)
 {
+    printf(">> data given is: <%d>\n", data);
+
     node_t *node = (node_t *)calloc(1, sizeof(node_t));
     if (!node)
     {
@@ -45,10 +51,46 @@ node_t *create_node(const char data, int data_type, node_t *left_node, node_t *r
     }
 
     node->data_type = data_type;
-    node->data.variable = data;
+    if (node->data_type == VARIABLE)
+        node->data.variable = data;
+    else if (node->data_type == OPERATION)
+        node->data.operation = data;
+    
     node->l = left_node;
     node->r = right_node;
+
+    node_print(node);
 
     return node;
 }
 
+void node_print(const node_t *node)
+{
+    assert(node);
+
+    setbuf(stdout, NULL);
+
+    printf(">> printing node number: %p\n", node);
+
+    switch (node->data_type)
+    {
+    case NUMBER:
+        printf("DATA: <%.2lf>\n", node->data.number);
+        break;
+    case OPERATION:
+        printf("DATA: <%#04x>\n", node->data.operation);
+        break;
+    case VARIABLE:
+        printf("DATA: <%c>\n", node->data.variable);
+        break;
+
+    default:
+        break;
+    }
+
+    printf("DATA_TYPE: <%d>\n", node->data_type);
+    printf("LEFT_PTR: <%p>\n", node->l);
+    printf("RIGHT_PTR: <%p>\n", node->r);
+    printf(">> node was printed.\n\n");
+    return;
+}
--- src/headers/create_node.h
+++ src/headers/create_node.h
@@ -4,12 +4,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "types.h"
 #include "operations.h"
 #include "log.h"
 
 node_t *create_node(double data, int data_type, node_t *left_node, node_t *right_node);
-node_t *create_node(enum OPERATIONS data, int data_type, node_t *left_node, node_t *right_node);
+//node_t *create_node(enum OPERATIONS data, int data_type, node_t *left_node, node_t *right_node);
 node_t *create_node(const char data, int data_type, node_t *left_node, node_t *right_node);
 
 #endif
