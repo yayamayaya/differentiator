db8bec8ee0b5c7ec39af7fee75e71a62fe3440d8
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-04 00:28:15 +0300

    полноценное дифференцирование

--- src/differentiation/differentiate.cpp
+++ src/differentiation/differentiate.cpp
@@ -5,7 +5,7 @@ node_t *differen(node_t *node)
     assert(node);
     //static node_t *root = node;
 
-    //create_gparh_code(root, DIFF_OP);
+    //_CREATE_GRAPH()
 
     switch (node->data_type)
     {
@@ -23,11 +23,22 @@ node_t *differen(node_t *node)
         case SUB:
             return create_node((unsigned char)node->data.operation, OPERATION, differen(node->l), differen(node->r));
 
-        case MULT: return _MULT_OP();
-
-        case DIV: return _DIV_OP();
-        
-        case POW: return _POW_OP();
+        case MULT:  return _MULT_OP();
+        case DIV:   return _DIV_OP();
+        case POW:   return _POW_OP();
+        case SIN:   return _SIN_OP();
+        case COS:   return _COS_OP();
+        case TAN:   return _TAN_OP();
+        case COT:   return _COT_OP();
+        case SH:    return _SH_OP();
+        case CH:    return _CH_OP();
+        case TH:    return _TH_OP();
+        case CTH:   return _CTH_OP();
+        case LN:    return _LN_OP();
+        case ASIN:  return _ASIN_OP();
+        case ACOS:  return _ACOS_OP();
+        case ATAN:  return _ATAN_OP();
+        case ACOT:  return _ACOT_OP();
 
         default:
             LOG(">>> fatal error occured: operation was not found: %#04x%40s\n", node->data.operation, "[error]");
