831507e43e21c63f9b1f4db4774dcbb166c9f767
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-07 01:38:08 +0300

    убрал ошибки в парсере, сам парсер начал распозновать больше

--- src/parsing/rec_parsing.cpp
+++ src/parsing/rec_parsing.cpp
@@ -1,5 +1,7 @@
 #include "parsing.h"
 
+node_t *write_operation(token_t *token_arr, int *pos);
+
 node_t *pars_expr(token_t *token_arr)
 {
     assert(token_arr);
@@ -21,6 +23,7 @@ node_t *pars_expr(token_t *token_arr)
 node_t *pars_mult(token_t *token_arr, int *pos)
 {
     assert(token_arr);
+    assert(pos);
 
     node_t *node = pars_power(token_arr, pos);
 
@@ -53,35 +56,15 @@ node_t *pars_power(token_t *token_arr, int *pos)
 node_t *pars_number(token_t *token_arr, int *pos)
 {
     assert(token_arr);
+    assert(pos);
     node_t *node = NULL;
 
     if (token_arr[*pos].data_type == OPERATION)
-    {
-        if (token_arr[*pos].data.operation == OP_BRACK)
-        {
-            (*pos)++;
-            node = pars_expr(token_arr);
-            if (token_arr[*pos].data.operation != CL_BRACK)
-                LOG(">>> closing bracket wasn't found, character is: %#04x%40s\n", token_arr[*pos].data.operation, "[error]");                    
-            else
-                (*pos)++;            
-
-            return node;
-        }
-        else
-        {
-            LOG("> function creation\n");
-            unsigned char operation = token_arr[*pos].data.operation;
-            (*pos)++;            
-            node = create_node(operation, OPERATION, pars_expr(token_arr), NULL);
-            return node;
-        }
-    }
+        node = write_operation(token_arr, pos);
     else if (token_arr[*pos].data_type == NUMBER)
     {
         node = create_node(token_arr[*pos].data.number, NUMBER, NULL, NULL);
         (*pos)++;
-        return node;
     }
     else if (token_arr[*pos].data_type == VARIABLE)
     {
@@ -89,22 +72,44 @@ node_t *pars_number(token_t *token_arr, int *pos)
         LOG(">> variable found: <%c>\n", variable);
         node = create_node(variable, VARIABLE, NULL, NULL);
         (*pos)++;
-        return node;
     }
+    else
+        LOG(">>> syntax error, parser couldn't parse a number%40s\n", "[error]");
 
     return node;
 }
 
-int tree_kill(node_t *node)
+node_t *write_operation(token_t *token_arr, int *pos)
 {
-    assert(node != NULL);
+    assert(token_arr);
+    assert(pos);
+    node_t *node = NULL;
 
-    if (node->l != NULL)
-        tree_kill(node->l);
-    if (node->r != NULL)
-        tree_kill(node->r);
+    switch (token_arr[*pos].data.operation)
+    {
+    case OP_BRACK:
+        (*pos)++;
+        node = pars_expr(token_arr);
+        if (token_arr[*pos].data.operation != CL_BRACK)
+            LOG(">>> closing bracket wasn't found, character is: %#04x%40s\n", token_arr[*pos].data.operation, "[error]");                    
+        else
+            (*pos)++;            
 
-    free(node);
-
-    return 0;
+        return node;
+    
+    case CL_BRACK:
+        LOG(">>> lishnee closing bracket, recheck your expression%40s", "[error]");
+        (*pos)++;
+        return write_operation(token_arr, pos);
+    
+    case SQRT:
+        (*pos)++;
+        return create_node((unsigned char)POW, OPERATION, pars_number(token_arr, pos), create_node((double)0.5, NUMBER, NULL, NULL));
+    
+    default:
+        LOG("> function creation\n");
+        unsigned char operation = token_arr[*pos].data.operation;
+        (*pos)++;            
+        return create_node(operation, OPERATION, pars_number(token_arr, pos), NULL);
+    }
 }
