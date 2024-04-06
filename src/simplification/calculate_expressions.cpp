47e90dbe433b3cd8d568825e65121e64c60c0ec1
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-07 01:39:05 +0300

    калькулятор в процессе работы над ошибками

--- src/simplification/calculate_expressions.cpp
+++ src/simplification/calculate_expressions.cpp
@@ -38,24 +38,12 @@ node_t *calculate_tree(node_t *node)
     }
     else if (node->data_type == OPERATION)
     {
-        node_t *leftNode = calculate_tree(node->l);
-        node_t *rightNode = calculate_tree(node->r);
+        node_t *left_node = calculate_tree(node->l);
+        node_t *right_node = calculate_tree(node->r);
 
-        LOG("> Nodes on node %p was calculated\n", node);
-        
-        if (!leftNode || !rightNode)
-        {
-            LOG("> One of the child nodes returned NULL, operation will not be executed\n");
+        if (make_operation(left_node, right_node, node))
             return NULL;
-        }
-
-        double result = make_operation(leftNode->data.number, rightNode->data.number, node->data.operation);
-        LOG("> result of calc is: %lf\n", result);
-
-        op_to_num_node(node);
-        LOG("> node was translated to number\n");
-        node->data.number = result;
-
+              
         create_gparh_code(root, OP);
 
         return node;
@@ -64,24 +52,75 @@ node_t *calculate_tree(node_t *node)
    return NULL;
 }
 
-double make_operation(double first_arg, double second_arg, char operation_number)
+int make_operation(node_t *left_node, node_t *right_node, node_t *node)
 {
-    switch (operation_number)
+    assert(node->data_type == OPERATION);
+
+    double first_arg = 0;
+    double second_arg = 0;
+    double result = 0;
+
+    /*if (left_node)
+        first_arg = left_node->data.number;
+    if (right_node)
+        second_arg = right_node->data.number;*/
+    
+    LOG("left node pointer is: %p\n", left_node);
+    LOG("right node pointer is: %p\n", right_node);
+    LOG("operation number is: %d\n", node->data.operation);
+    //LOG("logical ex[]")
+
+    if ((!(node->data.operation & 0xF0)) && (left_node && right_node))
     {
-    case ADD:
-        return first_arg + second_arg;
-    case SUB:
-        return first_arg - second_arg;
-    case MULT:
-        return first_arg * second_arg;
-    case DIV:
-        return first_arg / second_arg;
-    case POW:
-        return pow(first_arg, second_arg);
-    default:
-        LOG(">>> fatal error in making an operation%40s\n", "[error]");
-        return 0;
+        first_arg = left_node->data.number;
+        second_arg = right_node->data.number;
+
+        switch (node->data.operation)
+        {
+        case ADD:   result = first_arg + second_arg; break;
+        case SUB:   result = first_arg - second_arg; break;
+        case MULT:  result = first_arg * second_arg; break;
+        case DIV:   result = first_arg / second_arg; break;
+        case POW:   result = pow(first_arg, second_arg); break;
+
+        default:
+            LOG(">>> fatal error in making an operation%40s\n", "[error]");
+            return 1;
+        }
     }
+    else if (left_node)
+    {
+        first_arg = left_node->data.number;
+        switch (node->data.operation)
+        {
+        case LN:    result = log(first_arg); break;
+        case LG:    result = log10(first_arg); break;
+        case SIN:   result = sin(first_arg); break;
+        case COS:   result = cos(first_arg); break;
+        case TAN:   result = tan(first_arg); break;
+        case COT:   result = 1 / tan(first_arg); break;
+        case SH:    result = sinh(first_arg); break;
+        case CH:    result = cosh(first_arg); break;
+        case TH:    result = tanh(first_arg); break;
+        case CTH:   result = atanh(first_arg); break;
+        case ASIN:  result = asin(first_arg); break;
+        case ACOS:  result = acos(first_arg); break;
+        case ATAN:  result = atan(first_arg); break;
+        case ACOT:  result = M_PI_2 - atan(first_arg); break;
+
+        default:
+            LOG("unary command wasn't found\n");
+            return 0;
+        }
+    }
+
+    op_to_num_node(node);
+    LOG("> node was translated to number\n");
+    node->data.number = result;
+
+    LOG("> result of calc is: %lf\n", result);     
+        
+    return 0;
 }
 
 void op_to_num_node(node_t *node)
