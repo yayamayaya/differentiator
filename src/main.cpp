4f51de5069e501af9aaa64331b4882416586beaf
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-07 01:37:15 +0300

    добавлено упрощение выражений

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "headers/main.h"
 
+void simpl_expr(node_t **root);
+
 int main(int argc, char const *argv[])
 {
     _OPEN_LOG("log/diff_log.log");
@@ -18,16 +20,16 @@ int main(int argc, char const *argv[])
     node_t *root = pars_expr(token_arr);
     LOG("\n-> expression was parsed, root pointer is %p\n\n", root);
 
-    calculate(root);
+    simpl_expr(&root);
     LOG("\n-> pre-diff calculations were done\n\n");
     _CREATE_GRAPH(root, EXPR);
 
     node_t *diff_root = differen(root);
     LOG("\n-> expression was differentiated, diff. root pointer is %p\n\n", diff_root);
 
-    calculate(diff_root);
-    LOG("\n-> post-diff calculations were done\n\n");
     _CREATE_GRAPH(diff_root, DIFF_EXPR);
+    simpl_expr(&diff_root);
+    LOG("\n-> post-diff calculations were done\n\n");
 
     tree_kill(root);
     tree_kill(diff_root);
@@ -35,4 +37,16 @@ int main(int argc, char const *argv[])
     LOG("\n-> programm ended successfully");
     _CLOSE_LOG();
     return 0;
+}
+
+void simpl_expr(node_t **root)
+{
+    int flag = 0;
+    while (!flag)
+    {
+        flag = 1;
+        calculate(*root);
+        //*root = simplificate(*root, &flag);
+    }
+    return;
 }
