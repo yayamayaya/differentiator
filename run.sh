944f6ebe4586f7cf91e99c16adc4ee80862dd495
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-14 15:25:54 +0300

    -

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-valgrind -s --leak-check=full --show-leak-kinds=all build/main_src/differentiator $1 $2 $3
+valgrind -s --leak-check=full --show-leak-kinds=all build/main_src/differentiator $@
