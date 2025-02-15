7549955d15ba452b2183779d4076ef80a0c02f62
yayamayaya <semenov.aleksandr@phystech.edu>
2025-02-15 22:26:06 +0300

    вынес всё в отдельный класс дифференциатора

--- main_src/main.cpp
+++ main_src/main.cpp
@@ -1,63 +1,19 @@
-#include <iostream>
-#include "debugging.hpp"
-#include "tokenizator.hpp"
-#include "parser.hpp"
-#include "simplificate.hpp"
-#include "argv_parser.hpp"
-#include "calculator.hpp"
 #include "differentiator.hpp"
+#include <cmath>
 
 int main(int argc, char const *argv[])
 {
-    argv_st args = {argc, argv};
-    if (!args.check_parse_status()) return NOT_ENOUGH_ARGS;
+    if (argc != 2) return 1;
 
-    open_log();
-    LOG(LONG_LINE);
-    LOG("> starting programm...\n");
-#ifndef TESTING
-    clear_old_graphics();
-#endif
+    differentiator differ = {};
 
-    token_t *token_arr = NULL;
-    ret_t ret_val = tokenization::tokenizator(token_arr, args.give_expr());
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log());
+    // differ.turn_on_picture_printing("pic.png");
 
-    parser_ft parser = {token_arr};
-    node_t *root = parser.pars_expr();
-    _RETURN_ON_TRUE(!root, SYNTAX_ERR, close_log());
-    
-    simplificator simpl = {root};
-    root = simpl.simplificate();
-    _RETURN_ON_TRUE(simpl.check_for_zero_div(), WRONG_EXPR_ERR, close_log());
+    differ.differentiate(argv[1]);
 
-    calculator calc = {root};
-    ret_val = calc.run_calculator();
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log());
+    //std::cout << "result = " << differ.evaluate_x_value(3) << "\n";
 
-    root->create_gparh_code("before_diff_" + args.give_graphname());
-
-    differentiation_module differ = {root};
-    ret_val = differ.run();
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log());
-
-    node_t *diffed_root = differ.return_diffed_root();
-    
-    simplificator diff_simpl = {diffed_root};
-    diffed_root = diff_simpl.simplificate();
-    _RETURN_ON_TRUE(diff_simpl.check_for_zero_div(), WRONG_EXPR_ERR, close_log());
-    
-    diffed_root->create_gparh_code("after_diff_" + args.give_graphname());
-    
-    calculator diff_calc = {diffed_root};
-    ret_val = diff_calc.run_calculator();
-    _RETURN_ON_TRUE(ret_val, ret_val, close_log());
-    
-    
-    delete root;
-    delete diffed_root;
-
-    close_log();
+    differ.print_results(stdout);
 
     return 0;
 }
